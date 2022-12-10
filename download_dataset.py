799eb393a3957e43c358482dde6ae177f1ab4e83
badrmarani <badr-eddine.marani@centrale.centralelille.fr>
2022-12-10 15:50:16 +0100

    python script to download and unzip the dataset

---
+++ download_dataset.py
@@ -0,0 +1,20 @@
+from tqdm import tqdm
+import zipfile
+import gdown
+import os
+
+output_filename = "logo2k-dataset.zip"
+if not os.path.exists(output_filename):
+    print(">>> Downloading")
+    gdown.download(
+        url="https://drive.google.com/uc?export=download&id=1IFDF7gyjnnyrns4Fm-Ui8sMloBsNY1EO",
+        output=output_filename,
+        quiet=False,
+    )
+
+print(">>> Unzipping")
+zf = zipfile.ZipFile(output_filename)
+for file in tqdm(zf.infolist(), ascii=True):
+    zf.extract(file)
+
+os.remove(output_filename)
