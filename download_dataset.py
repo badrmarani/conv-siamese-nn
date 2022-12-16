a238ee40d363d3be073c4f4753794774175cbcd4
badrmarani <badr-eddine.marani@centrale.centralelille.fr>
2022-12-16 19:37:23 +0100

    minor update

--- download_dataset.py
+++ download_dataset.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
 import zipfile
+import shutil
 import gdown
 import os
 
@@ -19,3 +20,13 @@ for file in tqdm(zf.infolist(), ascii=True):
 
 os.remove(output_filename)
 os.rename("Logo-2K+/", "logo2k-dataset/")
+
+
+# rearrange the images
+if not os.path.exists("dataset/"):
+    os.makedirs("dataset/")
+
+path="logo2k-dataset/"
+for f in tqdm.tqdm(os.listdir(path=path), ascii=True):
+    for ff in os.listdir(os.path.join(path, f)):
+        shutil.move(os.path.abspath(path+f+"/"+ff), os.path.abspath("dataset/"))
